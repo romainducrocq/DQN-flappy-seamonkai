bb6df3d28b993a1554a8622d2261b78b1321b33d
romainducrocq <romain.g.ducrocq@gmail.com>
2021-06-05 14:22:16 +0200

    bin scripts

---
+++ bin/observe.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+function run () {
+
+python3 observe.py -dir ./save/PerDuelingDoubleDQNAgent_lr0.001_model.pack -max_steps 0
+
+}
+
+cd ..
+
+source venv/bin/activate
+
+run
+
+deactivate
+
+exit
