b89b9d1566c8ebba7c6a87c37b423647eb37a17a
romainducrocq <romain.g.ducrocq@gmail.com>
2021-07-23 12:39:05 +0200

    refactor custom dqn env

--- env/custom_env/dqn_env.py
+++ env/custom_env/dqn_env.py
@@ -1,8 +1,8 @@
-# """CHANGE DQN ENV TEMPLATE HERE""" ###################################################################################
-
+# """CHANGE CUSTOM ENV IMPORT HERE""" ##################################################################################
 from .pipe import Pipes
 from .seamonkey import SeaMonkey
 from .utils import RES
+########################################################################################################################
 
 
 class DqnEnv:
@@ -10,64 +10,89 @@ class DqnEnv:
     def min_max_scale(self, x, feature):
         return (x - self.min_max[feature][0]) / (self.min_max[feature][1] - self.min_max[feature][0])
 
-    def __init__(self):
+    def __init__(self, mode, player):
+        self.mode = mode
+        self.player = player
+
+        # """CHANGE ENV CONSTRUCT HERE""" ##############################################################################
         self.seamonkey = SeaMonkey()
         self.pipes = Pipes()
+        ################################################################################################################
 
+        # """CHANGE FEATURE SCALING HERE""" ############################################################################
         self.min_max = {
             "sonar_distance_x": (0., RES[0] + (self.pipes.pipes[0].w // 2)),
             "sonar_distance_y": (0., abs(self.seamonkey.y_lim[0]) + abs(self.pipes.pipes[0].y_lim[1])),
             "rel_h": (0., 1.),
             "rew": (0., 1.)
         }
+        ################################################################################################################
 
+        # """CHANGE ACTION AND OBSERVATION SPACE SIZES HERE""" #########################################################
         self.action_space_n = len(self.seamonkey.actions)
         self.observation_space_n = self.seamonkey.n_sonars + 1
+        ################################################################################################################
 
     def obs(self):
+        # """CHANGE OBSERVATION HERE""" ################################################################################
         x, y = self.pipes.get_next_pipe.end_x_y
         self.seamonkey.sonars(x, y)
         self.seamonkey.relative_height(y)
 
-        return [
+        obs = [
             self.min_max_scale(self.seamonkey.sonar_distances[0], "sonar_distance_x"),
             self.min_max_scale(self.seamonkey.sonar_distances[1], "sonar_distance_y"),
             self.min_max_scale(self.seamonkey.rel_h, "rel_h")
         ]
+        ################################################################################################################
+        return obs
 
     def rew(self):
+        # """CHANGE REWARD HERE""" #####################################################################################
         rew = pow(1 - self.min_max_scale(self.seamonkey.sonar_distances[1], "sonar_distance_y"), 2) / 10
 
         if self.pipes.passed_pipe(self.seamonkey.back_x()):
             self.seamonkey.reward()
             rew = 1
-
-        return self.min_max_scale(rew, "rew")
+        ################################################################################################################
+        return rew
 
     def done(self):
-        return self.seamonkey.is_collision(self.pipes.get_next_pipe.points())
+        # """CHANGE DONE HERE""" #######################################################################################
+        done = self.seamonkey.is_collision(self.pipes.get_next_pipe.points())
+        ################################################################################################################
+        return done
 
     def info(self):
-        return {
+        # """CHANGE INFO HERE""" #######################################################################################
+        info = {
             "time": round(self.seamonkey.get_time(), 2),
             "score": self.seamonkey.score
         }
+        ################################################################################################################
+        return info
 
     def reset(self):
+        # """CHANGE RESET HERE""" ######################################################################################
         self.seamonkey = SeaMonkey(lim_features=self.min_max)
         self.pipes = Pipes()
+        ################################################################################################################
 
     def step(self, action):
+        # """CHANGE STEP HERE""" #######################################################################################
         self.seamonkey.move(action)
         self.pipes.add_pipe()
         self.pipes.move_pipes()
         self.pipes.remove_pipe()
         self.pipes.next_pipe(self.seamonkey.back_x())
+        ################################################################################################################
 
     def reset_render(self):
+        # """CHANGE RESET RENDER HERE""" ###############################################################################
         pass
+        ################################################################################################################
 
     def step_render(self):
+        # """CHANGE STEP RENDER HERE""" ################################################################################
         self.seamonkey.rotate_theta()
-
-########################################################################################################################
+        ################################################################################################################
